5f57ab26ce318a8da65db41f2ebfad3914182244
Sahaj Rastogi <sahaj.rastogi@gmail.com>
2024-10-25 14:08:45 -0700

    placeholders2

---
+++ Geometry/icpc.cpp
@@ -0,0 +1,35 @@
+/*
+ID: sahajrastogi
+LANG: C++11
+*/
+
+#include <iostream>
+#include <bits/stdc++.h>
+#include <unordered_set>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+
+typedef long long ll;
+
+using namespace std;
+//using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define pb push_back
+#define f first
+#define s second
+#define int int64_t
+#define pi pair<int,int>
+#define pf pair<float,float>
+
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    #ifndef ONLINE_JUDGE
+    freopen("file.txt", "r", stdin);
+    #endif
+
+    
+    
+}
