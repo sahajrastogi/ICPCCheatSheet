70833a560427fe5092b28ed1b853bd51a51163dc
Sahaj Rastogi <sahaj.rastogi@gmail.com>
2024-10-25 15:46:56 -0700

    syntax and headers

---
+++ Syntax and Headers/CustomComparator.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    #ifndef ONLINE_JUDGE
+    freopen("file.txt", "r", stdin);
+    #endif
+
+    
+    
+}
