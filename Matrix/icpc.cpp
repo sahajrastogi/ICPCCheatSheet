e98674e19a45caabddd8ea22fd45d0d17de92f5e
Sahaj Rastogi <sahaj.rastogi@gmail.com>
2024-10-25 15:19:52 -0700

    matrix

--- Matrix/icpc.cpp
+++ Matrix/icpc.cpp
@@ -17,34 +17,30 @@ struct matrix {
             cout << endl;
         }
     }
-int x[MN][MN];
-
-matrix & operator *= (const matrix &o) {
-memset(x, 0, sizeof x);
-for (int i = 0; i < r; ++i)
-for (int k = 0; k < c; ++k)
-if (m[i][k] != 0)
-for (int j = 0; j < c; ++j) {
-x[i][j] = (x[i][j] + ((m[i][k] * o.m[k][j]) % mod) )
-% mod;
-}
-memcpy(m, x, sizeof(m));
-return *this;
-}
+    int x[MN][MN];
+    matrix & operator *= (const matrix &o) {
+        memset(x, 0, sizeof x);
+        for (int i = 0; i < r; ++i)
+            for (int k = 0; k < c; ++k)
+                if (m[i][k] != 0)
+                for (int j = 0; j < c; ++j) {
+                    x[i][j] = (x[i][j] + ((m[i][k] * o.m[k][j]) % mod))% mod;
+                }
+                memcpy(m, x, sizeof(m));
+        return *this;
+    }
 };
 void matrix_pow(matrix b, long long e, matrix &res) {
-memset(res.m, 0, sizeof res.m);
-for (int i = 0; i < b.r; ++i)
-res.m[i][i] = 1;
-if (e == 0) return;
-while (true) {
-if (e & 1) res *= b;
-if ((e >>= 1) == 0) break;
-b *= b;
+    memset(res.m, 0, sizeof res.m);
+    for (int i = 0; i < b.r; ++i)
+        res.m[i][i] = 1;
+    if (e == 0) return;
+    while (true) {
+        if (e & 1) res *= b;
+        if ((e >>= 1) == 0) break;
+    b *= b;
+    }
 }
-}
-
-
 
 signed main(){
     ios_base::sync_with_stdio(false);
