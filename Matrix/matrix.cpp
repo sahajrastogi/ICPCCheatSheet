cb1599b26725fbe2c743279460d77d418281877f
Sahaj Rastogi <sahaj.rastogi@gmail.com>
2024-10-29 18:08:09 -0700

    final matrix

--- Matrix/matrix.cpp
+++ Matrix/matrix.cpp
@@ -1,8 +1,9 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-const int MN = 111;
-const int mod = 10000;
+#define int long long
+const int MN = 205;
+const int mod = 998244353;
 
 struct matrix {
     int r, c;
@@ -17,18 +18,6 @@ struct matrix {
             cout << endl;
         }
     }
-    //int x[MN][MN];
-    // matrix & operator *= (const matrix &o) {
-    //     memset(x, 0, sizeof x);
-    //     for (int i = 0; i < r; ++i)
-    //         for (int k = 0; k < c; ++k)
-    //             if (m[i][k] != 0)
-    //             for (int j = 0; j < c; ++j) {
-    //                 x[i][j] = (x[i][j] + ((m[i][k] * o.m[k][j]) % mod))% mod;
-    //             }
-    //             memcpy(m, x, sizeof(m));
-    //     return *this;
-    // } //m*=n -> m=m*n
 
     matrix operator *(const matrix &b){
         matrix res(r, b.c);
@@ -44,49 +33,28 @@ struct matrix {
         return res;
     }
 
-    matrix operator *=(const matrix &b){
+    void operator *=(const matrix &b){
         *this = *this * b;
-        return *this;
+        //return *this;
     }
 
-    matrix operator ^(long long e){
+    matrix operator ^(int e){
         matrix res(r,r);
+        //matrix id(r,r);
         matrix b = *this;
         for (int i = 0; i < r; ++i)
             res.m[i][i] = 1;
-        if (e == 0) return;
+        if (e == 0) return res;
         while (true) {
             if (e & 1) res *= b;
             if ((e >>= 1) == 0) break;
             b *= b;
         }
+        return res;
     }
 
-    matrix operator ^=(long long e){
+    void operator ^=(int e){
         *this = *this ^ e;
-        return *this;
+        //return *this;
     }
 };
-void matrix_pow(matrix b, long long e, matrix &res) {
-    memset(res.m, 0, sizeof res.m);
-    for (int i = 0; i < b.r; ++i)
-        res.m[i][i] = 1;
-    if (e == 0) return;
-    while (true) {
-        if (e & 1) res *= b;
-        if ((e >>= 1) == 0) break;
-        b *= b;
-    }
-}
-
-signed main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    
-    #ifndef ONLINE_JUDGE
-    freopen("file.txt", "r", stdin);
-    #endif
-
-    
-    
-}
