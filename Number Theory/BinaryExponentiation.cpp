bab707c40117242fa93725fe2795441aa4b4bc64
Sahaj Rastogi <sahaj.rastogi@gmail.com>
2024-11-06 13:10:04 -0700

    rename

---
+++ Number Theory/BinaryExponentiation.cpp
@@ -0,0 +1,9 @@
+//switch * to + for safe ll multiplication
+int power(int b, int e, int m) {
+    int res = 1; 
+    while (e > 0) {
+        if (e & 1){
+            res = res * b;res%=m;
+        } b = b * b;b%= m;e >>= 1;
+    } return res;
+}
