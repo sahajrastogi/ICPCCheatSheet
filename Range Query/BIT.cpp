f220a4d71811e5d1ee2af20100ea14f612420b47
theodoregossett <theodoregossett@gmail.com>
2025-01-14 01:36:20 -0700

    Small changes
    Sparse table before was too long for no good reason

--- Range Query/BIT.cpp
+++ Range Query/BIT.cpp
@@ -2,10 +2,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 int sum(int i, vector<int> &bit){
-    int res = 0; while(i>=0) res+=bit[i]; i=((i+1)&i)-1; return res;
+    int res = 0; while(i>=0) {res+=bit[i]; i=((i+1)&i)-1;} return res;
 }
 void upd(int i, int wt, vector<int> &bit){
-    while(i<bit.size()) bit[i]+=wt; i=(i+1)|i;
+    while(i<bit.size()) {bit[i]+=wt; i=(i+1)|i;}
 }
 int range(int a, int b,vector<int>&bit){
     if(a == 0) return sum(b,bit); // care for indexing
--- Range Query/Sparse Table.cpp
+++ Range Query/Sparse Table.cpp
@@ -1,28 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int MN = 100000 + 10; // Max number of elements
-const int ML = 18; // ceil(log2(MN));
-struct st {
-    int data[MN];
-    int M[MN][ML];
-    int n;
-    void init(const vector<int> &d) {
-        n = d.size();
-        for (int i = 0; i < n; ++i)
-        data[i] = d[i];
-        build();
-    }
-    void build() {
-        for (int i = 0; i < n; ++i)
-        M[i][0] = data[i];
-        for (int j = 1, p = 2, q = 1; p <= n; ++j, p <<= 1, q <<= 1)
-        for (int i = 0; i + p- 1 < n; ++i)
-            M[i][j] = max(M[i][j- 1], M[i + q][j- 1]);
+
+int main(){
+    vector<int> a;
+    vector<vector<int>> Maxs(0, vector<int>(17, 0)); //arr.size, log2(arr.size);
+    for(int i=(int)a.size()-1;i>=0;i--){
+        Maxs[i][0] = 0; //fill in arr[i]
+        for(int j=0; i+(1<<(j+1))-1 <a.size() ;j++){
+            Maxs[i][j+1] = max(Maxs[i][j], Maxs[i+(1<<j)][j]);
         }
-        int query(int b, int e) {
-        int k = log2(e- b + 1);
-        return max(M[b][k], M[e + 1- (1<<k)][k]);
     }
+
+    // int query(int l, int r) {
+    // int k = log2(e- b + 1);
+    // return max(Maxs[b][k], Maxs[e + 1- (1<<k)][k]);
+    // }
 };
 
 
