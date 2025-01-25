e82521f3d4afa6c0524db39709d115cd41e6bb1e
BenjaminJeter <167565686+BenjaminJeter@users.noreply.github.com>
2025-01-25 13:42:08 -0700

    Create ben_macros.cpp

---
+++ Misc/ben_macros.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+#include<set>
+#include<map>
+#include<bitset>
+#include<unordered_map>
+#include<algorithm>
+#include<math.h>
+#include<queue>
+#include<iomanip>
+#include<unordered_set>
+#include<algorithm>
+#include<numeric>
+#include<stack>
+#include<random>
+
+using namespace std;
+
+#define ll long long
+#define pb push_back	
+#define mp make_pair
+#define F first
+#define S second
+#define V vector
+#define vi vector<int>
+#define vl vector<long long>
+#define vb vector<bool>
+#define vs vector<string>
+#define vd vector<double>
+#define pi pair<int,int>
+#define pl pair<long long, long long>
+#define all(i) i.begin(),i.end()
+#define REP(i,a,b) for(int i = a; i <= b; i++)
+#define PER(i,a,b) for(int i = a; i >= b; i--)
+#define sz(i) (int) i.size()
+#define smax(a,b) a=max(a,b)
+#define smin(a,b) a=min(a,b)
+#define int long long
+
+template<typename T>
+void printv(V<T> x){
+	for(T i : x)cout << i << " ";
+	cout << endl;
+}
