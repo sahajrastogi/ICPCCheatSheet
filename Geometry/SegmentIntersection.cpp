7f9f249093427b6ba1fde7b9cbf290b5037c0c36
BenjaminJeter <167565686+BenjaminJeter@users.noreply.github.com>
2025-02-09 16:54:10 -0700

    changed .f->.F and .s->.S

--- Geometry/SegmentIntersection.cpp
+++ Geometry/SegmentIntersection.cpp
@@ -3,15 +3,15 @@
 
 using namespace std;
 #define pb push_back
-#define f first
-#define s second
+#define F first
+#define S second
 #define int int64_t
 #define pi pair<int,int>
 
 
-pi operator-(const pi &l, const pi &r) { return {l.f - r.f, l.s - r.s}; }
-int norm(const pi &p) { return (p.f*p.f) + (p.s*p.s); }  // x^2 + y^2
-int cross(const pi &a, const pi &b) { return a.f * b.s - a.s * b.f; }  // cross product
+pi operator-(const pi &l, const pi &r) { return {l.F - r.F, l.S - r.S}; }
+int norm(const pi &p) { return (p.F*p.F) + (p.S*p.S); }  // x^2 + y^2
+int cross(const pi &a, const pi &b) { return a.F * b.S - a.S * b.F; }  // cross product
 int cross(const pi &p, const pi &a, const pi &b) {   // cross product
 	return cross(a - p, b - p);
 }
@@ -22,10 +22,10 @@ int sn(int x){
 }
 bool rect_int(pi p1, pi p2, pi p3, pi p4) {
 	int x1, x2, x3, x4, y1, y2, y3, y4;
-	x1 = min(p1.f, p2.f), x2 = max(p1.f, p2.f);
-	y1 = min(p1.s, p2.s), y2 = max(p1.s, p2.s);
-	x3 = min(p3.f, p4.f), x4 = max(p3.f, p4.f);
-	y3 = min(p3.s, p4.s), y4 = max(p3.s, p4.s);
+	x1 = min(p1.F, p2.F), x2 = max(p1.F, p2.F);
+	y1 = min(p1.S, p2.S), y2 = max(p1.S, p2.S);
+	x3 = min(p3.F, p4.F), x4 = max(p3.F, p4.F);
+	y3 = min(p3.S, p4.S), y4 = max(p3.S, p4.S);
 	return !(x2 < x3 || x4 < x1 || y2 < y3 || y4 < y1);
 }
 
